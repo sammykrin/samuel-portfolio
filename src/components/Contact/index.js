9bfe6b26c83474795fccb8dcd060dbc54ed3cf38
sammykrin <olamilehinsamuelolamide@gmail.com>
2024-03-08 11:59:43 -0800

    Added map and emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -21,10 +22,10 @@ const Contact = () => {
 
         emailjs
             .sendForm(
-                'gmail',
+                'service_fhqrta5',
                 'template_btnq9t8',
                 refForm.current,
-                'service_fhqrta5',
+                'xCF8_9Il8kQRZ-nCg'
             )
             .then(
                 () => {
@@ -57,13 +58,13 @@ const Contact = () => {
                         <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                               <li className='half'>
-                                <input type="text" name="name" placeholder="Name" required />
+                                <input type="text" name="name_id" placeholder="Name" required />
                               </li>  
                               <li className='half'>
-                                <input type="email" name="email" placeholder="Email" required />
+                                <input type="email" name="email_id" placeholder="Email" required />
                               </li>  
                               <li>
-                                <input placeholder="Subject" type="text" name="subject" required />
+                                <input placeholder="Subject" type="text" name="subject_id" required />
                               </li>  
                               <li>
                                   <textarea placeholder="Message" name="message" required></textarea>
@@ -74,6 +75,23 @@ const Contact = () => {
                             </ul>    
                         </form>    
                     </div>
+            </div>
+            <div className='info-map'>
+                Samuel Olamilehin,
+                <br />
+                Nigeria,
+                <br />
+                Peace Villa House, 210262 <br />
+                <span>olamilehinsamuelolamide@gmail.com</span>
+            </div>
+            <div className ='map-wrap'>
+                <MapContainer center={[8.1442896, 4.2296280]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={[8.1442896, 4.2296280]}>
+                    <Popup>Samuel lives here, come over for a cup of coffee :)</Popup>
+
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
         <Loader type="pacman" />
